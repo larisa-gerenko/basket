b4a848e6420e007c146c02534ab4f54e8b95cf72
Larisa <larisavalgutova@gmail.com>
2020-08-11 23:41:48 +0300

    Реализовать переключение карточки товара из обычного вида в расширенный и обратно при клике на любую область карточки товара. При клике на кнопку «Купить» открывать alert окно с текстом по шаблону «куплен товар <НАЗВАНИЕ_ТОВАРА>». Кнопка «сбросить» не активна, если ни одного фильтра не выбрано.

--- script.js
+++ script.js
@@ -1,78 +1,139 @@
-let goods = new Vue({
-    el: "#app",
-    data: {
-        brands: "",
-        sizes: "",
-        colors: "",
-        products: [
-            {
-                id: 1,
-                name: 'куртка красная',
-                img: 'https://media.istockphoto.com/photos/male-coat-isolated-on-the-white-picture-id163208487',
-                category: 'куртки',
-                oldPrice: 5880,
-                price: 4790,
-                brand: 'super',
-                size: 31,
-                color: 'красный'
-            },
-            {
-                id: 2,
-                name: 'куртка большая',
-                img: 'https://media.istockphoto.com/photos/red-womans-sports-jacket-picture-id520887025',
-                category: 'куртки',
-                oldPrice: 5900,
-                price: 3790,
-                brand: 'super',
-                size: 42,
-                color: 'зеленый'
-            },
-            {
-                id: 3,
-                name: 'куртка модная',
-                img: 'https://media.istockphoto.com/photos/male-coat-isolated-on-the-white-picture-id163208487',
-                category: 'куртки',
-                price: 5550,
-                brand: 'puper',
-                size: 29,
-                color: 'красный'
-            },
-            {
-                id: 4,
-                name: 'куртка выгодная',
-                img: 'https://media.istockphoto.com/photos/red-womans-sports-jacket-picture-id520887025',
-                category: 'куртки',
-                oldPrice: 7900,
-                price: 1990,
-                brand: 'super',
-                size: 29,
-                color: 'зеленый'
-            }
-        ]
+Vue.component("one-product", {
+  props: ["product"],
+  data: function () {
+    return {
+      visible: false,
+    };
+  },
+  template: `<div v-on:click = "visible = !visible"  class="card text-center border-0 shadow-sm mb-3 bg-white rounded-lg">
+                <img class="card-img-top"
+                     src="https://media.istockphoto.com/photos/male-coat-isolated-on-the-white-picture-id163208487">
+                <div class="card-body p-1 m-1">
+                    <h5 class="card-title mb-0">{{product.name}}</h5>
+                    <div class="card-text">
+                        <div>
+                            <small class="card-text text-muted">
+                                {{product.category}}
+                            </small>
+                        </div>
+                        <div>
+                            <del>{{product.oldPrice}}</del>
+                            <strong>{{product.price}} ₽</strong>
+                        </div>
+                        <div v-if = "visible" class="alert alert-dark p-0 m-0" role="alert">
+                            <div>
+                                <small>
+                                    <strong>Бренд</strong> {{product.brand}}
+                                </small>
+                            </div>
+                            <div>
+                                <small>
+                                    <strong>Размер</strong> {{product.size}}
+                                </small>
+                            </div>
+                            <div>
+                                <small>
+                                    <strong>Цвет</strong> {{product.color}}
+                                </small>
+                            </div>
+                        </div>
+                    </div>
+                    <a v-on:click = "buying(product)" href="#" class="btn btn-primary m-1">Купить</a>
+                </div>
+            </div>`,
+  methods: {
+    buying(product) {
+      alert(`куплен товар ${product.name}`);
     },
-computed: {
-        filteredProducts(){
-            console.log(this.brands, this.sizes, this.colors);
-            return this.products.filter((productsObj) => {
+  },
+});
 
-
-                if (this.brands && productsObj.brand !== this.brands.toLowerCase()) {
-                    return false;
-                }
-                if (this.sizes && productsObj.size !== parseInt(this.sizes)) {
-                    return false;
-                }
-                if (this.colors && productsObj.color !== this.colors.toLowerCase()) {
-                    return false;
-                }
-
-                return true;
-
-            })
+let goods = new Vue({
+  el: "#app",
+  data: {
+    brands: "",
+    sizes: "",
+    colors: "",
+    products: [
+      {
+        id: 1,
+        name: "куртка красная",
+        img:
+          "https://media.istockphoto.com/photos/male-coat-isolated-on-the-white-picture-id163208487",
+        category: "куртки",
+        oldPrice: 5880,
+        price: 4790,
+        brand: "super",
+        size: 31,
+        color: "красный",
+      },
+      {
+        id: 2,
+        name: "куртка большая",
+        img:
+          "https://media.istockphoto.com/photos/red-womans-sports-jacket-picture-id520887025",
+        category: "куртки",
+        oldPrice: 5900,
+        price: 3790,
+        brand: "super",
+        size: 42,
+        color: "зеленый",
+      },
+      {
+        id: 3,
+        name: "куртка модная",
+        img:
+          "https://media.istockphoto.com/photos/male-coat-isolated-on-the-white-picture-id163208487",
+        category: "куртки",
+        price: 5550,
+        brand: "puper",
+        size: 29,
+        color: "красный",
+      },
+      {
+        id: 4,
+        name: "куртка выгодная",
+        img:
+          "https://media.istockphoto.com/photos/red-womans-sports-jacket-picture-id520887025",
+        category: "куртки",
+        oldPrice: 7900,
+        price: 1990,
+        brand: "super",
+        size: 29,
+        color: "зеленый",
+      },
+    ],
+  },
+  computed: {
+    filteredProducts() {
+      console.log(this.brands, this.sizes, this.colors);
+      return this.products.filter((productsObj) => {
+        if (this.brands && productsObj.brand !== this.brands.toLowerCase()) {
+          return false;
+        }
+        if (this.sizes && productsObj.size !== parseInt(this.sizes)) {
+          return false;
+        }
+        if (this.colors && productsObj.color !== this.colors.toLowerCase()) {
+          return false;
         }
 
-}
+        return true;
+      });
+    },
+    isCleaning() {
+      if (this.brands || this.sizes || this.colors) {
+        return true;
+      }
 
-})
-
-
+      return false;
+    },
+  },
+  methods: {
+    cleaning() {
+      this.brands = "";
+      this.sizes = "";
+      this.colors = "";
+    },
+  },
+});
